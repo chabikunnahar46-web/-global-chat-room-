ef7e56679d28254db31b190ba9d3859d59794432
chabikunnahar46-web <chabikunnahar46@gmail.com>
2025-12-09 06:37:43 +0530

    Implement Firebase for chat message handling
    Integrate Firebase for real-time messaging functionality.

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
-let chatBox = document.getElementById("chat-box");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getFirestore, collection, addDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
-function sendMessage() {
-    let msg = document.getElementById("messageInput").value;
+// Your Firebase Config
+const firebaseConfig = {
+    apiKey: "AIzaSyBi7uoQT-2Lg-wlGMptk3Dryy43ZA2gpgk",
+    authDomain: "global-chat-75f38.firebaseapp.com",
+    projectId: "global-chat-75f38",
+    storageBucket: "global-chat-75f38.firebasestorage.app",
+    messagingSenderId: "682790896070",
+    appId: "1:682790896070:web:5d142dec99031730f072c7"
+};
 
-    if (msg.trim() === "") return;
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-    let div = document.createElement("div");
-    div.innerHTML = "<b>You:</b> " + msg;
-    div.style.margin = "10px 0";
-    
-    chatBox.appendChild(div);
+const chatBox = document.getElementById("chatBox");
+const sendBtn = document.getElementById("sendBtn");
+const input = document.getElementById("messageInput");
+
+// Send Message
+sendBtn.onclick = async () => {
+    const text = input.value.trim();
+    if (text === "") return;
+
+    await addDoc(collection(db, "messages"), {
+        text: text,
+        time: new Date()
+    });
+
+    input.value = "";
+};
+
+// Load Messages Live
+onSnapshot(collection(db, "messages"), (snapshot) => {
+    chatBox.innerHTML = "";
+
+    snapshot.forEach((doc) => {
+        const data = doc.data();
+        const msg = document.createElement("div");
+        msg.classList.add("message", "other-message");
+        msg.textContent = data.text;
+        chatBox.appendChild(msg);
+    });
+
     chatBox.scrollTop = chatBox.scrollHeight;
-
-    document.getElementById("messageInput").value = "";
-}
+});
