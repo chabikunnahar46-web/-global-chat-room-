6661016a028ae2f2c93ff68b3c2f6c29e4538115
chabikunnahar46-web <chabikunnahar46@gmail.com>
2025-12-12 07:58:02 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,16 +1,30 @@
-import { auth } from "./firebase.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+let ageInput = document.getElementById("age");
+let gfBox = document.getElementById("gf-box");
+let bankBox = document.getElementById("bank-box");
 
-document.getElementById("signupBtn").addEventListener("click", () => {
-  const email = document.getElementById("email").value;
-  const pass = document.getElementById("password").value;
+ageInput.addEventListener("input", () => {
+  let age = Number(ageInput.value);
 
-  createUserWithEmailAndPassword(auth, email, pass)
-    .then(() => {
-      alert("Account Created!");
-      window.location.href = "profile.html";
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
+  if (age >= 1 && age <= 24) {
+    gfBox.style.display = "block";
+    bankBox.style.display = "none";
+  } 
+  else if (age >= 25 && age <= 70) {
+    gfBox.style.display = "none";
+    bankBox.style.display = "block";
+  }
 });
+
+function createAccount() {
+  let name = document.getElementById("name").value;
+  let age = Number(document.getElementById("age").value);
+  let girlfriend = document.getElementById("girlfriend").value;
+  let balance = document.getElementById("balance").value;
+
+  let data = { name, age, girlfriend, balance };
+
+  localStorage.setItem("userProfile", JSON.stringify(data));
+
+  alert("Account Created!");
+  window.location.href = "profile.html";
+}
