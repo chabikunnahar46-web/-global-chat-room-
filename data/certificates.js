49f58e67d16c376f93e07912f53634beacceff8f
chabikunnahar46-web <chabikunnahar46@gmail.com>
2025-12-17 07:32:46 +0530

    Create certificates.js

---
+++ data/certificates.js
@@ -0,0 +1,20 @@
+<script>
+const certificates = [
+  {
+    id: "SZ-WK01-DR-001",
+    name: "Rahim Ahmed",
+    skill: "Drawing",
+    rank: "1st",
+    week: "Week 1",
+    date: "March 2025"
+  },
+  {
+    id: "SZ-WK01-WR-002",
+    name: "Sadia Khan",
+    skill: "Writing",
+    rank: "2nd",
+    week: "Week 1",
+    date: "March 2025"
+  }
+];
+</script>
