9f8a2e8b173b9eaa628cd0fee5dcb7173681e36c
chabikunnahar46-web <chabikunnahar46@gmail.com>
2025-12-19 06:56:59 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,23 +1,23 @@
-const dpImg = document.getElementById("dpImg");
-const coverImg = document.getElementById("coverImg");
-
-dpImg.src = localStorage.getItem("dp") || "";
-coverImg.src = localStorage.getItem("cover") || "";
-
-dpInput.onchange = e =>{
-  const reader = new FileReader();
-  reader.onload = ()=> {
-    dpImg.src = reader.result;
-    localStorage.setItem("dp", reader.result);
-  }
-  reader.readAsDataURL(e.target.files[0]);
+function goEdit() {
+  window.location.href = "edit_profile.html";
 }
 
-coverInput.onchange = e =>{
-  const reader = new FileReader();
-  reader.onload = ()=> {
-    coverImg.src = reader.result;
-    localStorage.setItem("cover", reader.result);
-  }
-  reader.readAsDataURL(e.target.files[0]);
-  }
+window.onload = () => {
+  if (localStorage.getItem("cover"))
+    document.getElementById("coverImg").src = localStorage.getItem("cover");
+
+  if (localStorage.getItem("dp"))
+    document.getElementById("dpImg").src = localStorage.getItem("dp");
+
+  if (localStorage.getItem("location"))
+    document.getElementById("location").innerText =
+      "🏠 " + localStorage.getItem("location");
+
+  if (localStorage.getItem("role"))
+    document.getElementById("role").innerText =
+      "🎓 " + localStorage.getItem("role");
+
+  if (localStorage.getItem("work"))
+    document.getElementById("work").innerText =
+      "💼 " + localStorage.getItem("work");
+};
