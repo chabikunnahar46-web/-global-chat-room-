c91a7e5621c4fa8b47d3e647a2d2c2893e48f730
chabikunnahar46-web <chabikunnahar46@gmail.com>
2025-12-20 21:28:02 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,16 @@
-<script type="module">
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBi7uoQT-2Lg-wlGMptk3Dryy43ZA2gpgk",
+  apiKey: "YOUR_API_KEY",
   authDomain: "global-chat-75f38.firebaseapp.com",
   databaseURL: "https://global-chat-75f38-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "global-chat-75f38",
   storageBucket: "global-chat-75f38.appspot.com",
-  messagingSenderId: "682790896070",
-  appId: "1:682790896070:web:5d142dec99031730f072c7"
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
-</script>
