11090765ca5124feec7a04cd7297387dbd666ada
chabikunnahar46-web <chabikunnahar46@gmail.com>
2025-12-21 03:45:51 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -42,3 +42,21 @@ onChildAdded(messagesRef, (snapshot) => {
   chatBox.appendChild(msgDiv);
   chatBox.scrollTop = chatBox.scrollHeight;
 });
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBi7uoQT-2Lg-wlGMptk3Dryy43ZA2gpgk",
+  authDomain: "global-chat-75f38.firebaseapp.com",
+  databaseURL: "https://global-chat-75f38-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "global-chat-75f38",
+  storageBucket: "global-chat-75f38.appspot.com",
+  messagingSenderId: "682790896070",
+  appId: "1:682790896070:web:5d142dec99031730f072c7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
