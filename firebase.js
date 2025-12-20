a2360808366c0fb399b18b6860a0c8165bf27185
chabikunnahar46-web <chabikunnahar46@gmail.com>
2025-12-21 03:43:45 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,18 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyBi7uoQT-2Lg-wlGMptk3Dryy43ZA2gpgk",
   authDomain: "global-chat-75f38.firebaseapp.com",
   databaseURL: "https://global-chat-75f38-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "global-chat-75f38",
   storageBucket: "global-chat-75f38.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  messagingSenderId: "682790896070",
+  appId: "1:682790896070:web:5d142dec99031730f072c7"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+export const database = getDatabase(app);
